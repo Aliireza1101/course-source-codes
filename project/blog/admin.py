f4c30d6729681905192f11fc4af91e890871d609
Alii <aliireza.dev@gmail.com>
2023-12-14 12:44:15 +0330

    Register post model in admin.py and customize

--- project/blog/admin.py
+++ project/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "publish_date", "status"]
+    list_editable = ["status"]
+    list_display_links = ["title", "author", "publish_date"]
+    list_filter = ["status", "publish_date", "author"]
+
+    ordering = ["create_date"]
+
+    prepopulated_fields = {"slug": ["title"]}
+    date_hierarchy = "create_date"
+
+    search_fields = ["title", "description", "author"]
+    raw_id_fields = ["author"]
