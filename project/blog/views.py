931092c6cb597d9b83663021dd97aeaf28f558cd
Alii <aliireza.dev@gmail.com>
2023-12-14 15:25:22 +0330

    Define example views for urls

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+def index(request: HttpRequest):
+    return HttpResponse("Developing ...")
+
+
+def postList(request: HttpRequest):
+    return HttpResponse("Developing ...")
+
+
+def postDetail(request: HttpRequest, pk: int):
+    return HttpResponse("Developing ...")
