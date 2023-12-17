9c402dc374d15cc411ff2b9093bd0ca1c5ddf346
Aliireza 1101 <aliireza.dev@gmail.com>
2023-12-17 13:11:28 +0330

    Define a validation for phone_number field in Ticket form

--- project/blog/forms.py
+++ project/blog/forms.py
@@ -10,8 +10,16 @@ class TicketForm(forms.Form):
 
     title = forms.CharField(max_length=255, required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
-    
+
     subject = forms.ChoiceField(choices=Subject.choices, required=True)
-    
+
     email = forms.EmailField()
     phone_number = forms.CharField(max_length=11)
+
+    def clean_phone_number(self) -> str:
+        phone_number: str = self.cleaned_data["phone_number"]
+        if phone_number:
+            if not phone_number.isnumeric():
+                raise forms.ValidationError("This field must be completely numeric!")
+            else:
+                return phone_number
