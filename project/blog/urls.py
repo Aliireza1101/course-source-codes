d5077e86a709d7473324d0f5b6eb2accea4750a7
Aliireza 1101 <aliireza.dev@gmail.com>
2023-12-20 09:20:53 +0330

    Define url for create post

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", view=views.index, name="index"),
     path("posts/", view=views.postList, name="post_list"),
     path("posts/<pk>/", view=views.postDetail, name="post_detail"),
+    path("create/", view=views.createPost, name="create_post"),
     path("posts/<pk>/add-comment", view=views.addComment, name="add_comment"),
     path("ticket/", view=views.createTicket, name="ticket"),
 ]
