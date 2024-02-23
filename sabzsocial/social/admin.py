7fafd766611d1c3ad27e472d77b45be523fc7e1e
Aliireza 1101 <aliireza.dev@gmail.com>
2024-02-23 18:15:10 +0330

    Custome abstract user

--- sabzsocial/social/admin.py
+++ sabzsocial/social/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ["username", "first_name", "last_name", "email", "phone"]
+    fieldsets = UserAdmin.fieldsets + (
+        ("Addiational Information", {"fields": ("bio", "date_of_birth", "photo", "job", "phone")}),
+    )
--- sabzsocial/social/models.py
+++ sabzsocial/social/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(verbose_name="بیوگرافی", blank=True, null=True)
+    date_of_birth = models.DateField(verbose_name="تاریخ تولد", blank=True, null=True)
+    photo = models.ImageField(verbose_name="عکس پروفایل", upload_to="profiles", blank=True, null=True)
+    job = models.CharField(verbose_name="شغل", max_length=255, blank=True, null=True)
+    phone = models.CharField(verbose_name="شماره تلفن", max_length=11, blank=True, null=True)
+
